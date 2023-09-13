ff64c097e2fdfe83664cf45c0d513549b266edc1
hectorlabra <121694325+hectorlabra@users.noreply.github.com>
2023-09-13 18:30:07 -0300

    diseño formulario y context

--- src/js/views/formulario.js
+++ src/js/views/formulario.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+import Navbar from "../component/navbar";
 
 export const Formulario = () => {
   const { store, actions } = useContext(Context);
@@ -30,69 +31,72 @@ export const Formulario = () => {
   };
 
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <h1 className="text-center m-3">AGREGA UN NUEVO CONTACTO</h1>
-          <label htmlFor="fullName" className="form-label">
-            Nombre Completo
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="fullName"
-            name="fullName"
-            value={formData.fullName}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="phone" className="form-label">
-            Telefono
-          </label>
-          <input
-            type="tel"
-            className="form-control"
-            id="phone"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="address" className="form-label">
-            Direccion
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="address"
-            name="address"
-            value={formData.address}
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Guardar
-        </button>
-      </form>
-      <br />
-      <Link to="/">
-        <button className="btn btn-primary">Volver a tus contactos</button>
-      </Link>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <form onSubmit={handleSubmit} className="mt-4">
+          <div className="mb-3">
+            <h1 className="text-center mb-3">Add a New Contact</h1>
+            <label htmlFor="fullName" className="form-label">
+              Full Name
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="fullName"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="phone" className="form-label">
+              Phone
+            </label>
+            <input
+              type="tel"
+              className="form-control"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="address" className="form-label">
+              Address
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </form>
+        <br />
+        <Link to="/">
+          <button className="btn btn-primary">Back to Your Contacts</button>
+        </Link>
+      </div>
+    </>
   );
 };
